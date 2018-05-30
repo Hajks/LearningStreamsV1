1ece7f89528ccb70a7ed0aaf7e5050074bc6a811
Hajk <tunczorx@gmail.com>
2018-05-30 15:01:15 +0200

    Dodaj pliki projektów.

---
+++ LearningStreamsV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningStreamsV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
